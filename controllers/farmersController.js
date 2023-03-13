92f16b1f4e11621d771e56b2929e2b5e8bdeb09a
rohandhananjaya <rohandhananjaya@gmail.com>
2023-03-14 00:01:36 +0530

    Basic routes added to farmers

--- controllers/farmersController.js
+++ controllers/farmersController.js
@@ -1,11 +1,50 @@
-const getFarmer = (req,res) =>{
+// @desc Get farmers
+// @route GET /api/farmers
+// @access Private
+const getFarmers = (req,res) =>{
     res.status(200).send(
         {
-            message : 'Get farmers 3.0'
+            message : 'Get farmers'
+        }
+    )
+}
+
+// @desc Add farmers
+// @route POST /api/farmers
+// @access Private
+const addFarmer = (req,res) =>{
+    res.status(200).send(
+        {
+            message : 'Add farmer'
+        }
+    )
+}
+
+// @desc Update farmer
+// @route PUT /api/farmers/:id
+// @access Private
+const updateFarmer = (req,res) =>{
+    res.status(200).send(
+        {
+            message : `Update farmer id : ${req.params.id}`
+        }
+    )
+}
+
+// @desc Delete farmer
+// @route DELETE /api/farmers/:id
+// @access Private
+const deleteFarmer = (req,res) =>{
+    res.status(200).send(
+        {
+            message : `Delete farmer id : ${req.params.id}`
         }
     )
 }
 
 module.exports = {
-    getFarmer,
+    getFarmers,
+    addFarmer,
+    updateFarmer,
+    deleteFarmer
 }
--- routes/routeFarmers.js
+++ routes/routeFarmers.js
@@ -1,32 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const {getFarmer} = require('../controllers/farmersController')
+const {
+    getFarmers, 
+    addFarmer, 
+    updateFarmer, 
+    deleteFarmer
+} = require('../controllers/farmersController')
 
-router.get('/', getFarmer)
+router.route('/').get(getFarmers).post(addFarmer)
 
-router.post('/',(req,res)=>{
-    res.status(200).send(
-        {
-            message : 'Add farmer'
-        }
-    )
-})
-
-router.put('/:id',(req,res)=>{
-    res.status(200).send(
-        {
-            message : `Update farmer id : ${req.params.id}`
-        }
-    )
-})
-
-router.delete('/:id',(req,res)=>{
-    res.status(200).send(
-        {
-            message : `Delete farmer id : ${req.params.id}`
-        }
-    )
-})
+router.route('/:id').put(updateFarmer).delete(deleteFarmer)
 
 module.exports = router
