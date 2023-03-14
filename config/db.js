cd90ff8a38d9409095eea24a661c1b81ce3f92d9
rohandhananjaya <rohandhananjaya@gmail.com>
2023-03-14 16:39:24 +0530

    Database connected

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const { default: mongoose } = require('mongoose')
+const mpngoose =  require('mongoose')
+const connectDB = async () =>{
+    try{
+        const conn = mongoose.connect(process.env.MONGO_URI)
+        console.log(`Connected to database`.cyan.underline)
+    }catch(error){
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
